98a3d5515141a5d42f4374b99d14988138e8aa19
ryanrgdavis <ryanrgdavis@gmail.com>
2023-06-28 13:56:21 +0930

    JobApplication.js achieves requirements & specifications

--- driving-crooner/src/pages/JobApplication.js
+++ driving-crooner/src/pages/JobApplication.js
@@ -1,50 +1,76 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/JobApplication.css'
 
 function JobApplication() {
     const [submitted, setSubmitted] = useState(false);
-    const [volunteer, setVolunteer] = useState(false);
+    const [applyAgain, setApplyAgain] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (submitted) {
-            setVolunteer(true);
-        } else {
-            setSubmitted(true);
-        }
+        setSubmitted(true);
     };
 
-    return (
-        <div className="job-application-container">
-            <nav className="navbar"></nav>
-            <h1>The Driving Crooner</h1>
-            <h2>Job Application</h2>
-            {submitted ? (
-                volunteer ? (
-                    <p>Your job application has been submitted as a volunteer role. Thank you!</p>
-                ) : (
-                    <p>Your job application has already been submitted. Thank you!</p>
-                )
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" />
-                    </label>
-                    <br />
-                    <label>
-                        Email:
-                        <input type="email" />
-                    </label>
-                    <br />
-                    <label>
-                        Phone:
-                        <input type="tel" />
-                    </label>
-                    <br />
+    const handleApplyAgain = () => {
+        setApplyAgain(true);
+    };
+
+    const handleReset = () => {
+        setSubmitted(false);
+        setApplyAgain(true);
+    };
+
+    if (submitted && !applyAgain) {
+        return (
+            <div className="centre">
+                <h1>REJECTED</h1>
+                <p>GET OUT OF HERE! BUT ALSO THEN AGAIN WE DO NEED PEOPLE TO WORK FOR US. YOU SHOULD APPLY AGAIN, please!</p>
+                <button onClick={handleApplyAgain}>Apply Again</button>
+            </div>
+        );
+    }
+
+    if (!submitted && applyAgain) {
+        return (
+            <div className="job-application">
+                <p>Your job application has been submitted as a volunteer role. Thank you!</p>
+            </div>
+        )
+    }
+
+    if (submitted && applyAgain) {
+        return (
+            <div className="job-application">
+                <h1>The Driving Crooner</h1>
+                <h2>Job Application</h2>
+                <form onSubmit={handleReset}>
+                    <h2>Driver Job Application</h2>
+                    <input type="text" id="name" required />
+
+                    <input type="email" id="email" required />
+
+                    <input type="tel" id="phone" required />
+
                     <button type="submit">Submit</button>
                 </form>
-            )}
+            </div>
+        )
+    }
+
+    return (
+        <div className="job-application">
+            <h1>The Driving Crooner</h1>
+            <h2>Job Application</h2>
+            <form onSubmit={handleSubmit}>
+                <h2>Driver Job Application</h2>
+                <input type="text" id="name" required />
+
+                <input type="email" id="email" required />
+
+                <input type="tel" id="phone" required />
+
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
